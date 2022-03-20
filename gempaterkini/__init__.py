2e18a9e66efd68b16972a6e3114bc262d25350eb
WildanAruman <wildan.aruman90@gmail.com>
2022-03-20 13:25:49 +0700

    APLIKASI DETEKSI GEMPA MODULARISASI DENGAN PACKAGE

---
+++ gempaterkini/__init__.py
@@ -0,0 +1,20 @@
+def ekstraksi_data():
+    hasil = dict()
+    hasil['tanggal'] = '24 agustus 2022'
+    hasil['waktu'] = '12:05:52 WIB'
+    hasil['magnitudo'] = 4.0
+    hasil['lokasi'] = {'ls': 1.48, 'BT': 134.01}
+    hasil['pusat'] = 'Pusat gempa beradada di darat 19 km barat laut aceh'
+    hasil['dirasakan'] = 'Dirasakan (Skala MMI): II-III Manokwari, II-III Aceh'
+
+    return hasil
+
+
+def tampilkan_data(result):
+    print('Gempa terakhir berdasarkan data BMKG')
+    print(f"Tanggal, {result['tanggal']}")
+    print(f"waktu, {result['waktu']}")
+    print(f"Magnitudo, {result['magnitudo']}")
+    print(f"Lokasi: LS = {result['lokasi']['ls']}, BT = {result['lokasi']['ls']} ")
+    print(f"Pusat, {result['pusat']}")
+    print(f"Dirasakan, {result['dirasakan']}")
--- main.py
+++ main.py
@@ -1,30 +1,9 @@
 """
 APLIKASI DETEKSI GEMPA
 MODULARISASI DENGAN FUNCTION
+MODULARISAI DENGAN PACKAGE
 """
-
-
-def ekstraksi_data():
-    hasil = dict()
-    hasil['tanggal'] = '24 agustus 2022'
-    hasil['waktu'] = '12:05:52 WIB'
-    hasil['magnitudo'] = 4.0
-    hasil['lokasi'] = {'ls': 1.48, 'BT': 134.01}
-    hasil['pusat'] = 'Pusat gempa beradada di darat 19 km barat laut aceh'
-    hasil['dirasakan'] = 'Dirasakan (Skala MMI): II-III Manokwari, II-III Aceh'
-
-    return hasil
-
-
-def tampilkan_data(result):
-    print('Gempa terakhir berdasarkan data BMKG')
-    print(f"Tanggal, {result['tanggal']}")
-    print(f"waktu, {result['waktu']}")
-    print(f"Magnitudo, {result['magnitudo']}")
-    print(f"Lokasi: LS = {result['lokasi']['ls']}, BT = {result['lokasi']['ls']} ")
-    print(f"Pusat, {result['pusat']}")
-    print(f"Dirasakan, {result['dirasakan']}")
-
+from gempaterkini import ekstraksi_data, tampilkan_data
 
 if __name__ == '__main__':
     print('Aplikasi utama')
