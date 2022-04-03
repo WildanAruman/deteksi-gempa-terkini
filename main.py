0ccf7d9f10d4ac54de7ffa479f4af9a5cabf10fe
WildanAruman <wildan.aruman90@gmail.com>
2022-04-03 07:17:30 +0700

    finish real data from BMKG.GO.ID

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ MODULARISAI DENGAN PACKAGE
 """
 import gempaterkini
 if __name__ == '__main__':
-    print('Aplikasi utama')
+    print(f'Aplikasi utama menggunkan package yang memiliki deskripsi {gempaterkini.description}')
     result = gempaterkini.ekstraksi_data()
     gempaterkini.tampilkan_data(result)
