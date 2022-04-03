f7c9db4c72ca9f6d45c96a0fab5868907883e02d
WildanAruman <wildan.aruman90@gmail.com>
2022-04-03 07:34:37 +0700

    final version with oop

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ MODULARISASI DENGAN FUNCTION
 MODULARISAI DENGAN PACKAGE
 
 """
+#from gempaterkini import GempaTerkini
 import gempaterkini
 if __name__ == '__main__':
-    print(f'Aplikasi utama menggunkan package yang memiliki deskripsi {gempaterkini.description}')
-    result = gempaterkini.ekstraksi_data()
-    gempaterkini.tampilkan_data(result)
+    gempa_di_indeonesia = gempaterkini.GempaTerkini('https://bmkg.go.id')
+    print(f'Aplikasi utama menggunkan package yang memiliki deskripsi {gempa_di_indeonesia.description}')
 
+    gempa_di_indeonesia.tampilkan_keterangan()
+    gempa_di_indeonesia.run()
 
